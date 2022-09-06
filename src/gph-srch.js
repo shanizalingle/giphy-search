87684ba64ff70f1467e4a2a1a35d79311285c959
Noah Lundquist <nalundquist@gmail.com>
2022-09-06 15:14:05 -0700

    add html, WIP three apiget functions, css
    Co-authored-by: Shaniza Lingle <shanizalingle@gmail.com>

--- src/gph-srch.js
+++ src/gph-srch.js
@@ -1,7 +1,32 @@
-export function getGiphy(query){
-	let request = new XMLHttpRequest();
-	const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_Key}&q=${query}&limit=25&offset=0&rating=g&lang=en`;
+import { printElements } from "./index.js";
+import { printError } from "./index.js";
 
-	
+export function getTrendingGiphy(){
+	let trendingRequest = new XMLHttpRequest();
+	const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=10&rating=g`
 }
+
+export function getRandomGiphy(){
+	let randomRequest = new XMLHttpRequest();
+	const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=&rating=g`
+}
+
+export function getSearchGiphy(query){
+	let searchRequest = new XMLHttpRequest();
+	const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`;
+
+	request.addEventListener("loadend", function() {
+    const response = JSON.parse(this.responseText);
+		if (this.status === 200){
+			printElements(response, query);
+		} else {
+			printError(this, response, query);
+		}
+  })
+
+	searchRequest.open("GET", url, true);
+	searchRequest.send();
+}
+
+
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { getGiphy } from "./gph-srch.js";
+
+export function printElements(apiResponse, query){
+	const giphyResult = document.getElementById("giphy-result");
+	document.getElementById("giphy-result").innerHTML = null;
+	const queryDisplay = document.createElement("h2");
+	queryDisplay.append(query);
+	giphyResult.append(queryDisplay);
+  
+	for (let i=0; i < apiResponse.data.length; i++){
+		let gifResult = apiResponse.data[i].images.fixed_height.url;
+    let newImg = document.createElement("img");
+    newImg.setAttribute("src", gifResult);
+    giphyResult.append(newImg);
+	}
+}
+
+export function printError(request, apiResponse, query){
+	const giphyResult = document.getElementById("giphy-result");
+	document.getElementById("giphy-result").innerHTML = null;
+	const queryDisplay = document.createElement("h2");
+	queryDisplay.append(`${query} was unable to display due to the following error: ${request.status} ${request.statusText}: ${apiResponse.message}`);
+	giphyResult.append(queryDisplay);
+}
+
+function formSubmit(event){
+	event.preventDefault();
+	const query = document.getElementById("gif-search").value;
+	document.querySelector("#gif-search").value = null;
+	getGiphy(query); 
+}
+
+window.addEventListener("load", function(){
+	document.querySelector('form').addEventListener("submit", formSubmit);
+})
