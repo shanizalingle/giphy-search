584ff76da848976944991ef54c5ba16e50c52073
Shaniza Lingle <shanizalingle@gmail.com>
2022-09-09 12:04:55 -0700

    add css

--- src/gph-srch.js
+++ src/gph-srch.js
@@ -6,7 +6,7 @@ import { printError } from "./index.js";
 
 export function getTrendingGiphy(){
 	let trendingRequest = new XMLHttpRequest();
-	const trendingURL = `https://api.giphy.com/v1/gifs/trending?api_key=P9L5r8zyDwrySaO0tjCla06Q2ec7Npqp&limit=12&rating=g`
+	const trendingURL = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=12&rating=g`
 
 	trendingRequest.addEventListener("loadend", function(){
 		const response = JSON.parse(this.responseText);
@@ -21,7 +21,7 @@ export function getTrendingGiphy(){
 
 export function getRandomGiphy(){
 	let randomRequest = new XMLHttpRequest();
-	const randomURL = `https://api.giphy.com/v1/gifs/random?api_key=P9L5r8zyDwrySaO0tjCla06Q2ec7Npqp&tag=&rating=g`
+	const randomURL = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=&rating=g`
 
 	randomRequest.addEventListener("loadend", function(){
 			const response = JSON.parse(this.responseText);
@@ -36,7 +36,7 @@ export function getRandomGiphy(){
 
 export function getSearchGiphy(query){
 	let searchRequest = new XMLHttpRequest();
-	const searchURL = `https://api.giphy.com/v1/gifs/search?api_key=P9L5r8zyDwrySaO0tjCla06Q2ec7Npqp&q=${query}&limit=15&offset=0&rating=g&lang=en`;
+	const searchURL = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=15&offset=0&rating=g&lang=en`;
 
 	searchRequest.addEventListener("loadend", function(){
 			const response = JSON.parse(this.responseText);
